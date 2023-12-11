c0ea0fe60363a6c3f9d27372f5d3490ce359a139
Temirlan <210107026@stu.sdu.edu.kz>
2023-12-11 17:16:30 +0600

    second commit

--- CoffeeMachine/main.php
+++ CoffeeMachine/main.php
@@ -18,9 +18,7 @@ class Menu {
     public $menu;
 
     public function __construct(array $items){
-        foreach ($items as $item){
-            $this->menu[] = $item;
-        }
+            $this->menu = $items;
     }
 
     public function get_items():string{
@@ -41,17 +39,21 @@ class Menu {
 }
 
 class CoffeeMaker {
-    public $water;
-    public $coffee;
+    private $water;
+    private $coffee;
+    private $milk;
 
-    public function __construct(int $water,int $coffee)
+    public function __construct(int $water,int $coffee,int $milk)
     {
         $this->water = $water;
         $this->coffee = $coffee;
+        $this->milk = $milk;
     }
 
     public function report():string{
-        return "\nWater: ".$this->water." ml"."\nCoffee: ".$this->coffee." ml\n";
+        return "\nWater: ".$this->water." ml".
+               "\nCoffee: ".$this->coffee." gm".
+               "\nMilk: ".$this->milk." ml\n";
     }
 
     public function is_resource_sufficient(MenuItem $drink):bool{
@@ -63,13 +65,17 @@ class CoffeeMaker {
             echo "Not enough coffee";
             return false;
         }
+        if($this->milk<$drink->ingredients["milk"]){
+            echo "Not enough milk";
+            return false;
+        }
         return true;
     }
 
-    public function make_coffee(MenuItem $drink,float $cost,MoneyMachine $machine){
+    public function make_coffee(MenuItem $drink){
         $this->water -= $drink->ingredients["water"];
         $this->coffee -= $drink->ingredients["coffee"];
-        $machine->profit += $cost;
+        $this->milk -= $drink->ingredients["milk"];
     }
 }
 
@@ -84,13 +90,14 @@ class MoneyMachine{
         return "Money: ".$this->profit."$";
     }
 
-    public function make_payment(float $money,float $cost,float $change):bool{
+    public function make_payment(float $money,float $cost):bool{
         if($money>=$cost){
             echo "Payment accepted\n";
             if($money > $cost) {
                 $change = $money - $cost;
                 echo "Here is {$change} dollars in change";
             }
+            $this->profit += $cost;
             return true;
         }
         else echo "Sorry that's not enough money. Money refunded\n";
@@ -98,18 +105,20 @@ class MoneyMachine{
     }
 }
 
-$drink1 = new MenuItem("latte",10,["water"=>100,"coffee"=>10]);
-$drink2 = new MenuItem("espresso",20,["water"=>150,"coffee"=>20]);
-$drink3 = new MenuItem("cappuccino",15,["water"=>120,"coffee"=>15]);
+$drink1 = new MenuItem("latte",10,["water"=>100,"coffee"=>10,"milk"=>50]);
+$drink2 = new MenuItem("espresso",20,["water"=>150,"coffee"=>20,"milk"=>10]);
+$drink3 = new MenuItem("cappuccino",15,["water"=>120,"coffee"=>15,"milk"=>20]);
 
 $menu = new Menu([$drink1,$drink2,$drink3]);
-$coffeeMaker = new CoffeeMaker(1000,500);
-$coffeeCanBeMade = false;
-$machine = new MoneyMachine();
-$change = 0;
+$coffeeMaker = new CoffeeMaker(1000,500,700);
 
+$machine = new MoneyMachine();
+$count = 0;
 $run = true;
+
 while($run){
+    $count++;
+    $coffeeCanBeMade = false;
     $drink = readline("What would you like? {$menu->get_items()}}: ");
 
     if($drink=="off") break;
@@ -126,14 +135,14 @@ while($run){
     if($coffeeCanBeMade) {
         $money = (float)readline("Enter your money: ");
 
-        if($machine->make_payment($money,$drink->cost,$change)){
+        if($machine->make_payment($money,$drink->cost)){
             echo "{$n}\nReport before making coffee: ".$coffeeMaker->report();
-            if($machine->profit>0) echo $machine->report();
+            if($count>1) echo $machine->report();
 
-            $coffeeMaker->make_coffee($drink,$drink->cost,$machine);
+            $coffeeMaker->make_coffee($drink);
 
             echo "{$n}\nReport after making coffee: ".$coffeeMaker->report();
-            if($machine->profit>0) echo $machine->report();
+            if($count>1) echo $machine->report();
 
             echo "{$n}\nHere is your {$drink->name}. Enjoy!\n";
         }
